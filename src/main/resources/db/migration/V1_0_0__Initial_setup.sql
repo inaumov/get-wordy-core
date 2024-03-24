8f94109d046e51327691b3591be6ed9ca9a1d84c
Igor Naumov <naumov@live.com>
2024-03-25 00:05:53 +0200

    Migration to PostgreSQL
    * Migration to postgres 15.6; Bump lib version to 2.1.x
    
    * Solve the “must appear in the GROUP BY clause or be used in an aggregate function” Error

--- src/main/resources/db/migration/V1_0_0__Initial_setup.sql
+++ src/main/resources/db/migration/V1_0_0__Initial_setup.sql
@@ -1,46 +1,47 @@
-CREATE TABLE dictionaries (
-  id   SERIAL,
-  name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE dictionaries
+(
+    id   SERIAL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE words (
-  id            SERIAL,
-  word          VARCHAR(255) NOT NULL,
-  part_of_speech VARCHAR (40),
-  transcription VARCHAR(255),
-  meaning       VARCHAR(4000),
-  PRIMARY KEY (id)
+CREATE TABLE words
+(
+    id             SERIAL,
+    word           VARCHAR(255) NOT NULL,
+    part_of_speech VARCHAR(40),
+    transcription  VARCHAR(255),
+    meaning        VARCHAR(4000),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE cards (
-  id            SERIAL,
-  status        ENUM ('EDIT', 'POSTPONED', 'TO_LEARN', 'LEARNT')  NOT NULL DEFAULT 'EDIT',
-  score         SMALLINT DEFAULT '0'                              NOT NULL,
-  create_time   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  update_time   TIMESTAMP,
-  dictionary_id BIGINT UNSIGNED                                   NOT NULL,
-  word_id       BIGINT UNSIGNED                                   NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (word_id) REFERENCES words (id)
+CREATE TABLE cards
+(
+    id            SERIAL,
+    status        VARCHAR(20) DEFAULT 'EDIT' NOT NULL CHECK (status IN ('EDIT', 'POSTPONED', 'TO_LEARN', 'LEARNT')),
+    score         SMALLINT    DEFAULT 0      NOT NULL,
+    create_time   TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
+    update_time   TIMESTAMP,
+    dictionary_id BIGINT                     NOT NULL,
+    word_id       BIGINT                     NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (word_id) REFERENCES words (id) ON UPDATE CASCADE
 );
 
-CREATE TABLE context (
-  id               SERIAL,
-  example          VARCHAR(255),
-  card_id          BIGINT UNSIGNED NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (card_id) REFERENCES cards (id)
-    ON delete CASCADE
-    ON update CASCADE
+CREATE TABLE context
+(
+    id      SERIAL,
+    example VARCHAR(255) NOT NULL,
+    card_id BIGINT       NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (card_id) REFERENCES cards (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE collocations (
-  id            SERIAL,
-  example       VARCHAR(255),
-  card_id       BIGINT UNSIGNED NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (card_id) REFERENCES cards (id)
-    ON delete CASCADE
-    ON update CASCADE
+CREATE TABLE collocations
+(
+    id      SERIAL,
+    example VARCHAR(255) NOT NULL,
+    card_id BIGINT       NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (card_id) REFERENCES cards (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
